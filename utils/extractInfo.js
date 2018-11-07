4a7c1e376657dc2e481afc1494d52a1c32c5fdf4
yunyou <1205361247@qq.com>
2018-11-07 15:16:52 +0800

    feat: 1. add type arraypattern in variale declaration 2. traverse all versions

--- utils/extractInfo.js
+++ utils/extractInfo.js
@@ -4,10 +4,9 @@ const extract = require('babel-extract-comments');
 const babelParser = require('babylon');
 const babelTraverse = require('@babel/traverse').default
 const stringify = require('csv-stringify')
-const vueSrc = '/Users/wendahuang/Desktop/vue/';
-let srcDir = path.resolve(vueSrc, 'src')
+const blackList = ['.DS_Store']
+let res = []
 
-const blackList = ['.DS_Store'], res = []
 /*
 description: 提取文件中的注释和标识符
  */
@@ -27,11 +26,18 @@ function extractFileInfo(fpath) {
                 let { declarations } = node
                 for (let i = 0, len = declarations.length; i < len; i++) {
                     switch (declarations[i].id.type) {
-                        // 解构赋值
+                        // 对象解构赋值
                         case 'ObjectPattern':
-                            const { properties } = declarations[i].id
-                            properties.forEach(({ key }) => {
+                            const props = declarations[i].id.properties
+                            props.forEach(({ key }) => {
                                 identifiers.push(key.name)
+                            })
+                            break;
+                        //  数组结构赋值
+                        case 'ArrayPattern':
+                            const elems = declarations[i].id.elements
+                            elems.forEach(({ name }) => {
+                                identifiers.push(name)
                             })
                             break;
                         default:
@@ -71,20 +77,26 @@ function extractFileInfo(fpath) {
 }
 
 /*
-@desc 递归地遍历文件夹
+@desc 获取该目录下所有文件的文字信息
  */
-function traverseDir(dir) {
-    const files = fs.readdirSync(dir)
-    files.forEach(function (file, index) {
-        if (blackList.indexOf(file) !== -1) return
-        var curPath = path.resolve(dir, file),
-            info = fs.statSync(curPath)
-        if (info.isDirectory()) {
-            traverseDir(curPath);
-        } else {
-            extractFileInfo(curPath)
-        }
-    })
+function extractText(rootPath) {
+    function traverseDir(dir) {
+        const files = fs.readdirSync(dir)
+        files.forEach(function (file, index) {
+            if (blackList.indexOf(file) !== -1) return
+            var curPath = path.resolve(dir, file),
+                info = fs.statSync(curPath)
+            if (info.isDirectory()) {
+                traverseDir(curPath);
+            } else {
+                extractFileInfo(curPath)
+            }
+        })
+    }
+    res = []
+    traverseDir(path.resolve(rootPath, 'src'))
+    const seg = rootPath.split('/'), dirName = seg[seg.length - 1]
+    write2Csv(res, dirName)
 }
 
 /**
@@ -104,20 +116,33 @@ function formatIdentifier(id) {
 /*
 @desc 将对象转成csv格式并写入文件
  */
-function write2Csv(res) {
+function write2Csv(res, fileName) {
+    console.log('writing:', fileName)
     stringify(res, {
         header: true
     }, (err, data) => {
         // console.log(data)
-        fs.writeFileSync('/Users/wendahuang/Desktop/data/vue-1.0.20.csv', data)
-        console.log("finish writing")
+        fs.writeFileSync(`/Users/wendahuang/Desktop/data/${fileName}.csv`, data)
+        console.log("finish writing:",fileName)
     })
 }
 
 // extractFileInfo('../mock/commentId.js')
 
-traverseDir(srcDir)
-write2Csv(res)
+function main() {
+    const vueSrc = '/Users/wendahuang/Desktop/vue-all-versions',
+        files = fs.readdirSync(vueSrc)
+    let fpath = null
+    for (let i = 0, len = files.length; i < len; i++) {
+        fpath = path.resolve(vueSrc, files[i])
+        let stat = fs.statSync(fpath)
+        // console.log(fpath)
+        stat.isDirectory() && extractText(fpath)
+    }
+}
+main()
+/* traverseDir(srcDir)
+write2Csv(res) */
 
 // console.log(res)
 
