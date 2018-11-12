da80e7b7344a94ceb83c6b2a344ba2a918ccd052
yunyou <1205361247@qq.com>
2018-11-12 18:44:30 +0800

    feat: add route getTopicData

--- routes/topics.js
+++ routes/topics.js
@@ -3,15 +3,22 @@ var router = express.Router();
 var fs = require('fs');
 var parse = require('csv-parse/lib/sync')
 var path = require('path')
-const topicData = getTopicData()
+const fileData = getFileData(),topicData=getTopicData()
 
 /* GET home page. */
 router.get('/getAllDocs', function (req, res, next) {
     res.send({
-        files: topicData,
+        files: fileData,
         versions: getVersions()
     })
 });
+
+/**
+ * @description 获取主题的关键词
+ */
+router.get('/getTopicData',function(req,res,next){
+    res.send(topicData)
+})
 
 /**
  * @description 根据版本号获取主题在文件中的分布
@@ -19,7 +26,7 @@ router.get('/getAllDocs', function (req, res, next) {
 router.get('/getTopicDisByVersion', function (req, res, next) {
     const verReg = /vue-(\d*\.\d*\.\d*)/
     const version = req.query.version,
-        filteredTopicData = topicData.filter(d => d.filename.match(verReg)[1] === version)
+        filteredTopicData = fileData.filter(d => d.filename.match(verReg)[1] === version)
     let directory = path.resolve('/Users/wendahuang/Desktop/vue-all-versions', `vue-${version}`, 'src'),
         root = {
             name: directory,
@@ -77,10 +84,14 @@ function getVersions() {
     return versions
 }
 
-function getTopicData() {
+function getFileData() {
     const text = fs.readFileSync('/Users/wendahuang/Desktop/data/vue-all-versions-topic.csv', 'utf-8')
     return parse(text, {
         columns: true
     })
+}
+
+function getTopicData(){
+    return Array(10).fill(null)
 }
 module.exports = router;
